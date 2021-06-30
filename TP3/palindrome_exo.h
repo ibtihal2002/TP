932b8a643ef81a65d9fdd74aa225ab8423114cd7
ibtihal el banouri <80362007+ibtihal2002@users.noreply.github.com>
2021-06-30 21:05:31 +0100

    Update palindrome_exo.h

--- TP3/palindrome_exo.h
+++ TP3/palindrome_exo.h
@@ -1,40 +1,27 @@
+#ifndef PALINDROME_EXO_H
+#endif PALINDROME_EXO_H
+
+
 #include<stdio.h>
 #include<stdlib.h>
 
 
-
-
-
-int palindrom(int ac, char *av)
+struct node
 {
-	int i=0;
-	int j=strlen(av);
-	while (i<=j)
+	char ch;
+	struct node *next;
+};typedef struct node node;
+
+
+node add(char ch)
 {
-		if(av[i]!=av[j])
-			return 0;
-		i++;
-		j--;
-	}
-	return 1;	
+	node newnode=new node();
+	newnode.ch=ch;
+	newnode.next=NULL;
+	return newnode;
 }
 
 
 
-node string_linkedlist(char ch,node head)
-{
-	text=add(ch[0]);
-	node curr = head;
-	for (int i=1;i<strlen(ch);i++)
-	{
-		curr.next=add(text[i]);
-		curr = curr.next;
-	}
-	return head;
-}
-int main()
-{
-	int ac;
-	char *av[];
-	palid=palindrom(ac,av[1]);
-}
+
+
