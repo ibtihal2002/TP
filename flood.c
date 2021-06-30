7d32344ace3d41cae691a907a587d0a66aa9c95a
ibtihal el banouri <80362007+ibtihal2002@users.noreply.github.com>
2021-06-30 20:50:42 +0100

    Update flood.c

--- flood.c
+++ flood.c
@@ -43,13 +43,3 @@ void print_array(char **arr, t_point dim)
 			printf("%c",arr [i][j]);
 	}
 }
-
-int main()
-{
-	t_point dim;
-	scan_dim(&dim);
-	arr = allocated_array(dim);
-	fill_array(arr,dim);
-	print_array(arr,dim);
-	return 0;
-}
